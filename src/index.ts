2eaac218f1eab59afd7d314f3860ff779a047cb0
Mohaned Altater <altaterm@gmail.com>
2024-12-08 14:59:22 +0800

    product feature

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
-
+import { seedInitialProducts } from "./services/productService";
+import productRoute from "./routes/productRoute"
 const app=express()
 const port =3001
 
@@ -13,7 +14,11 @@ mongoose.connect("mongodb://127.0.0.1/ecommerce")
 .then(() => console.log("Mongo connected!"))
 .catch((err)=> console.log("failed to connect ", err))
 
+
+// seed the product to database
+seedInitialProducts();
 app.use('/user',userRoute)
+app.use('/product',productRoute)
 
 app.listen(port, ()=> {
     console.log('server is running at : http://localhost:3001')
---
+++ src/services/productService.ts
@@ -0,0 +1,27 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+    return await productModel.find();
+}
+
+
+export const seedInitialProducts =async ()=>{
+    const products =[
+        { title: "Dell Laptop", image: "https://benson.ph/cdn/shop/products/peripherals_laptop_latitude_3420nt_gallery_1.jpg?v=1627523235&width=3319", price:3000, stock:10},
+        // { title: "laptop 2", image: "image2.jpg", price:20, stock:10},
+        // { title: "mobile 3", image: "image3.jpg", price:10, stock:50},
+        // { title: "headPhone 4", image: "image4.jpg", price:30, stock:30},
+        // { title: "ear phone 5", image: "image5.jpg", price:40, stock:40},
+        // { title: "Product 6", image: "image6.jpg", price:50, stock:20},
+        // { title: "Product 7", image: "image7.jpg", price:160, stock:70},
+        // { title: "Product 8", image: "image8.jpg", price:50, stock:50},
+        // { title: "Product 9", image: "image9.jpg", price:20, stock:80},
+        // { title: "Product 10", image: "image10.jpg", price:60, stock:10},
+    ];
+
+    const existingProducts =await getAllProducts();
+
+    if(existingProducts.length===0){
+        await productModel.insertMany(products)
+    }
+};
