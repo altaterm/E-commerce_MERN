b5fe75d7b1cc9793ec001f5a4d9e8326d3a5e0ea
Mohaned Altater <altaterm@gmail.com>
2025-05-19 22:41:05 +0800

    logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -17,7 +17,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Navbar() {
-  const {username, isAuthenticated }= useAuth();
+  const {username, isAuthenticated, logout }= useAuth();
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
   const navigate = useNavigate();
@@ -33,6 +33,12 @@ function Navbar() {
   const handleLogin =()=>{
     navigate('/login');
   };
+
+  const handleLogout=()=>{
+    logout();
+    navigate('/');
+    handleCloseUserMenu();
+  }
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -87,7 +93,7 @@ function Navbar() {
                <MenuItem  onClick={handleCloseUserMenu}>
                   <Typography sx={{ textAlign: 'center' }}>My Orders</Typography>
                 </MenuItem>
-                <MenuItem  onClick={handleCloseUserMenu}>
+                <MenuItem  onClick={handleLogout}>
                   <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
                 </MenuItem>
             </Menu>
--- frontend/src/context/Auth/AuthProvider.tsx
+++ frontend/src/context/Auth/AuthProvider.tsx
@@ -1,24 +1,31 @@
 import {  useState, type FC, type PropsWithChildren } from "react";
 import { AuthContext } from "./AuthContext";
 
-
+const USERNAME_KEY ='username';
+const TOKEN_KEY ='token';
 
 const AuthProvider: FC<PropsWithChildren> = ({children}) =>{
 
-    const [username, setUserName]= useState <string | null >(localStorage.getItem('username'))
+    const [username, setUserName]= useState <string | null >(localStorage.getItem(USERNAME_KEY))
     const [token, setToken]= useState <string | null >(localStorage.getItem('token'))
-
+   const isAuthenticated = !!token;
 
     const login = (username: string, token : string )=> {
         setUserName(username);
         setToken(token);
-        localStorage.setItem('username', username);
-        localStorage.setItem('token', token);
+        localStorage.setItem(USERNAME_KEY, username);
+        localStorage.setItem(TOKEN_KEY, token);
     }
 
-    const isAuthenticated = !!token;
+    const logout =()=>{
+        localStorage.removeItem(USERNAME_KEY);
+        localStorage.removeItem(TOKEN_KEY);
+        setUserName(null);
+        setToken(null);
+    }
+ 
     return (
-        <AuthContext.Provider value={{username, token, login, isAuthenticated}}>
+        <AuthContext.Provider value={{username, token, isAuthenticated, login, logout}}>
             {children}
         </AuthContext.Provider>
     )
